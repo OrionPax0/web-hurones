5a1d9ac5a8062aec04fc060961174b3a4ca4e3df
OrionPax0 <165099441+OrionPax0@users.noreply.github.com>
2024-03-26 17:35:42 -0600

    Add files via upload

---
+++ estilos.css
@@ -0,0 +1,17 @@
+<script>
+    function printFlower() {
+        const flower = `
+            🌸🌸<br>
+       🌸🌸🌸🌸🌸🌸<br>
+    🌸🌸🌸🌸🌸🌸🌸🌸🌸<br>
+       🌸🌸🌸🌸🌸🌸<br>
+            🌸🌸<br>
+        `;
+        document.getElementById('flower').innerHTML = flower;
+    }
+
+    printFlower();
+</script>
+
+</body>
+</html>
